1f3dfea117cba9b1bab4204d0a8111cd58a84f59
Allan Arceno <arcenoallan214@gmail.com>
2025-03-04 00:00:15 +0800

    2024-03-04: java/python - update code coverage 005

--- python/test/test_math_operations.py
+++ python/test/test_math_operations.py
@@ -1,5 +1,5 @@
 import unittest
-from app.math_operations import add, subtract, multiply, divide
+from app.math_operations import add, subtract, multiply#, divide
 
 class TestMathOperations(unittest.TestCase):
     def test_add(self):
@@ -11,10 +11,10 @@ class TestMathOperations(unittest.TestCase):
     def test_multiply(self):
         self.assertEqual(multiply(2, 3), 6)
 
-    def test_divide(self):
-        self.assertEqual(divide(6, 2), 3)
-        self.assertEqual(divide(5, 2), 2.5)
-
-    def test_divide_by_zero(self):
-        with self.assertRaises(ValueError):
-            divide(5, 0)
+#    def test_divide(self):
+#        self.assertEqual(divide(6, 2), 3)
+#        self.assertEqual(divide(5, 2), 2.5)
+#
+#    def test_divide_by_zero(self):
+#        with self.assertRaises(ValueError):
+#            divide(5, 0)
